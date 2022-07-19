ec78ff489fb67501357d3acd297a893a45bb7f22
Rakuto-NOV <okudarakuto@gmail.com>
2022-07-19 16:49:59 +0900

    Button can be clicked

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,8 @@
 class Task < ApplicationRecord
+  has_one_attached :image
+  belongs_to :user
+
+  with_options presence: true do
+    validates :name, :content, :prioity_id, :daedline_month, :daedline_day
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   get 'tasks/index'
   root to: 'tasks#index'
-  resources :tasks, only: :index
+  resources :tasks, only: [:index, :new, :create]
 end
